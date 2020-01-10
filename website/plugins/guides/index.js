25d989e82f989f8dda7499634220d76b351b112c
Ashley Jeffs <ash@jeffail.uk>
2020-01-10 18:40:49 +0000

    WIP

--- website/plugins/guides/index.js
+++ website/plugins/guides/index.js
@@ -16,42 +16,16 @@ const DEFAULT_OPTIONS = {
   path: 'guides', // Path to data on filesystem, relative to site dir.
   routeBasePath: 'guides', // URL Route.
   include: ['*.md', '*.mdx'], // Extensions to include.
-  postsPerPage: 10, // How many posts per page.
-  guideListComponent: '@theme/BlogListPage',
+  guideListComponent: '@theme/GuideListPage',
   guidePostComponent: '@theme/BlogPostPage',
-  guideTagsListComponent: '@theme/BlogTagsListPage',
-  guideTagsPostsComponent: '@theme/BlogTagsPostsPage',
   remarkPlugins: [],
   rehypePlugins: [],
   truncateMarker: /<!--\s*(truncate)\s*-->/, // string or regex
 };
 
-function assertFeedTypes(val) {
-  if (typeof val !== 'string' && !['rss', 'atom', 'all'].includes(val)) {
-    throw new Error(
-      `Invalid feedOptions type: ${val}. It must be either 'rss', 'atom', or 'all'`,
-    );
-  }
-}
-
-const getFeedTypes = (type) => {
-  assertFeedTypes(type);
-  let feedTypes = [];
-
-  if (type === 'all') {
-    feedTypes = ['rss', 'atom'];
-  } else {
-    feedTypes.push(type);
-  }
-  return feedTypes;
-};
-
 module.exports = pluginContentGuide;
 
-function pluginContentGuide(
-  context,
-  opts,
-) {
+function pluginContentGuide(context, opts) {
   const options = {...DEFAULT_OPTIONS, ...opts};
   const {siteDir, generatedFilesDir} = context;
   const contentPath = path.resolve(siteDir, options.path);
@@ -71,8 +45,6 @@ function pluginContentGuide(
 
     // Fetches guide contents and returns metadata for the necessary routes.
     async loadContent() {
-      const {postsPerPage, routeBasePath} = options;
-
       const guidePosts = await generateGuidePosts(contentPath, context, options);
       if (!guidePosts) {
         return null;
@@ -97,87 +69,8 @@ function pluginContentGuide(
         }
       });
 
-      // Guide pagination routes.
-      // Example: `/guide`, `/guide/page/1`, `/guide/page/2`
-      const totalCount = guidePosts.length;
-      const numberOfPages = Math.ceil(totalCount / postsPerPage);
-      const {
-        siteConfig: {baseUrl = ''},
-      } = context;
-      const basePageUrl = normalizeUrl([baseUrl, routeBasePath]);
-
-      const guideListPaginated = [];
-
-      function guidePaginationPermalink(page) {
-        return page > 0
-          ? normalizeUrl([basePageUrl, `page/${page + 1}`])
-          : basePageUrl;
-      }
-
-      for (let page = 0; page < numberOfPages; page += 1) {
-        guideListPaginated.push({
-          metadata: {
-            permalink: guidePaginationPermalink(page),
-            page: page + 1,
-            postsPerPage,
-            totalPages: numberOfPages,
-            totalCount,
-            previousPage: page !== 0 ? guidePaginationPermalink(page - 1) : null,
-            nextPage:
-              page < numberOfPages - 1
-                ? guidePaginationPermalink(page + 1)
-                : null,
-          },
-          items: guidePosts
-            .slice(page * postsPerPage, (page + 1) * postsPerPage)
-            .map(item => item.id),
-        });
-      }
-
-      const guideTags = {};
-      const tagsPath = normalizeUrl([basePageUrl, 'tags']);
-      guidePosts.forEach(guidePost => {
-        const {tags} = guidePost.metadata;
-        if (!tags || tags.length === 0) {
-          // TODO: Extract tags out into a separate plugin.
-          // eslint-disable-next-line no-param-reassign
-          guidePost.metadata.tags = [];
-          return;
-        }
-
-        // eslint-disable-next-line no-param-reassign
-        guidePost.metadata.tags = tags.map(tag => {
-          if (typeof tag === 'string') {
-            const normalizedTag = _.kebabCase(tag);
-            const permalink = normalizeUrl([tagsPath, normalizedTag]);
-            if (!guideTags[normalizedTag]) {
-              guideTags[normalizedTag] = {
-                name: tag.toLowerCase(), // Will only use the name of the first occurrence of the tag.
-                items: [],
-                permalink,
-              };
-            }
-
-            guideTags[normalizedTag].items.push(guidePost.id);
-
-            return {
-              label: tag,
-              permalink,
-            };
-          } else {
-            return tag;
-          }
-        });
-      });
-
-      const guideTagsListPath =
-        Object.keys(guideTags).length > 0 ? tagsPath : null;
-
       return {
         guidePosts,
-        guideListPaginated,
-        guideTags,
-        guideTagsListPath,
       };
     },
 
@@ -189,8 +82,6 @@ function pluginContentGuide(
       const {
         guideListComponent,
         guidePostComponent,
-        guideTagsListComponent,
-        guideTagsPostsComponent,
       } = options;
 
       const aliasedSource = (source) =>
@@ -198,17 +89,12 @@ function pluginContentGuide(
       const {addRoute, createData} = actions;
       const {
         guidePosts,
-        guideListPaginated,
-        guideTags,
-        guideTagsListPath,
       } = content;
-
-      const guideItemsToMetadata = {};
 
       // Create routes for guide entries.
       await Promise.all(
         guidePosts.map(async guidePost => {
-          const {id, metadata} = guidePost;
+          const {metadata} = guidePost;
           await createData(
             // Note that this created data path must be in sync with metadataPath provided to mdx-loader
             `${docuHash(metadata.source)}.json`,
@@ -223,108 +109,42 @@ function pluginContentGuide(
               content: metadata.source,
             },
           });
-
-          guideItemsToMetadata[id] = metadata;
         }),
       );
+
+      const {routeBasePath} = options;
+      const {
+        siteConfig: {baseUrl = ''},
+      } = context;
+      const basePageUrl = normalizeUrl([baseUrl, routeBasePath]);
 
       // Create routes for guide's paginated list entries.
-      await Promise.all(
-        guideListPaginated.map(async listPage => {
-          const {metadata, items} = listPage;
-          const {permalink} = metadata;
-          const pageMetadataPath = await createData(
-            `${docuHash(permalink)}.json`,
-            JSON.stringify(metadata, null, 2),
-          );
-
-          addRoute({
-            path: permalink,
-            component: guideListComponent,
-            exact: true,
-            modules: {
-              items: items.map(postID => {
-                const metadata = guideItemsToMetadata[postID];
-                // To tell routes.js this is an import and not a nested object to recurse.
-                return {
-                  content: {
-                    __import: true,
-                    path: metadata.source,
-                    query: {
-                      truncated: true,
-                    },
-                  },
-                };
-              }),
-              metadata: aliasedSource(pageMetadataPath),
-            },
-          });
-        }),
+      // TODO: Need this?
+      const listPageMetadataPath = await createData(
+        `${docuHash(`${basePageUrl}`)}.json`,
+        JSON.stringify({}, null, 2),
       );
-
-      // Tags.
-      if (guideTagsListPath === null) {
-        return;
-      }
-
-      const tagsModule = {};
-
-      await Promise.all(
-        Object.keys(guideTags).map(async tag => {
-          const {name, items, permalink} = guideTags[tag];
-
-          tagsModule[tag] = {
-            allTagsPath: guideTagsListPath,
-            slug: tag,
-            name,
-            count: items.length,
-            permalink,
-          };
-
-          const tagsMetadataPath = await createData(
-            `${docuHash(permalink)}.json`,
-            JSON.stringify(tagsModule[tag], null, 2),
-          );
-
-          addRoute({
-            path: permalink,
-            component: guideTagsPostsComponent,
-            exact: true,
-            modules: {
-              items: items.map(postID => {
-                const metadata = guideItemsToMetadata[postID];
-                return {
-                  content: {
-                    __import: true,
-                    path: metadata.source,
-                    query: {
-                      truncated: true,
-                    },
-                  },
-                };
-              }),
-              metadata: aliasedSource(tagsMetadataPath),
-            },
-          });
-        }),
-      );
-
-      // Only create /tags page if there are tags.
-      if (Object.keys(guideTags).length > 0) {
-        const tagsListPath = await createData(
-          `${docuHash(`${guideTagsListPath}-tags`)}.json`,
-          JSON.stringify(tagsModule, null, 2),
-        );
-
-        addRoute({
-          path: guideTagsListPath,
-          component: guideTagsListComponent,
-          exact: true,
-          modules: {
-            tags: aliasedSource(tagsListPath),
-          },
-        });
-      }
+      addRoute({
+        path: basePageUrl,
+        component: guideListComponent,
+        exact: true,
+        modules: {
+          items: guidePosts.map(guidePost => {
+            const {metadata} = guidePost;
+            // To tell routes.js this is an import and not a nested object to recurse.
+            return {
+              content: {
+                __import: true,
+                path: metadata.source,
+                query: {
+                  truncated: true,
+                },
+              },
+            };
+          }),
+          metadata: aliasedSource(listPageMetadataPath),
+        },
+      });
     },
 
     configureWebpack(
@@ -372,85 +192,6 @@ function pluginContentGuide(
             },
           ],
         },
-      };
-    },
-
-    async postBuild({outDir}) {
-      if (!options.feedOptions) {
-        return;
-      }
-
-      const feed = await generateGuideFeed(context, options);
-
-      if (!feed) {
-        return;
-      }
-
-      const feedTypes = getFeedTypes((options.feedOptions || {}).type);
-
-      await Promise.all(
-        feedTypes.map(feedType => {
-          const feedPath = path.join(
-            outDir,
-            options.routeBasePath,
-            `${feedType}.xml`,
-          );
-          const feedContent = feedType === 'rss' ? feed.rss2() : feed.atom1();
-          return fs.writeFile(feedPath, feedContent, err => {
-            if (err) {
-              throw new Error(`Generating ${feedType} feed failed: ${err}`);
-            }
-          });
-        }),
-      );
-    },
-
-    injectHtmlTags() {
-      if (!options.feedOptions) {
-        return {};
-      }
-
-      const feedTypes = getFeedTypes((options.feedOptions || {}).type);
-      const {
-        siteConfig: {title},
-        baseUrl,
-      } = context;
-      const feedsConfig = {
-        rss: {
-          type: 'application/rss+xml',
-          path: 'guide/rss.xml',
-          title: `${title} Guide RSS Feed`,
-        },
-        atom: {
-          type: 'application/atom+xml',
-          path: 'guide/atom.xml',
-          title: `${title} Guide Atom Feed`,
-        },
-      };
-      const headTags = [];
-
-      feedTypes.map(feedType => {
-        const feedConfig = feedsConfig[feedType] || {};
-
-        if (!feedsConfig) {
-          return;
-        }
-
-        const {type, path, title} = feedConfig;
-
-        headTags.push({
-          tagName: 'link',
-          attributes: {
-            rel: 'alternate',
-            type,
-            href: normalizeUrl([baseUrl, path]),
-            title,
-          },
-        });
-      });
-
-      return {
-        headTags,
       };
     },
   };
